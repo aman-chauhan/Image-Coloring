f8fc245cec4c2eef7d03b7bc5cef571d712db58f
Aman Chauhan <amanchauhan1994@gmail.com>
2018-06-01 13:49:31 -0400

    Switch to PIL, change in data directory structure

--- convert.py
+++ convert.py
@@ -1,8 +1,4 @@
-from skimage.transform import resize
-from imageio import imread, imwrite
-from skimage import img_as_ubyte
-from skimage import color
-from pprint import pprint
+from PIL import Image
 import numpy as np
 import json
 import sys
@@ -19,7 +15,8 @@ def main(source, dest):
                     partition[root.split(os.sep)[6]] = []
                 partition[root.split(os.sep)[6]].extend([os.path.join(root, x)
                                                          for x in files if x.split('.')[1] == 'jpg'])
-                labels.update({x: '_'.join(root.split(os.sep)[8:]) for x in files if x.split('.')[1] == 'jpg'})
+                labels.update({x.split('.')[0] + '.png': '_'.join(root.split(os.sep)[8:])
+                               for x in files if x.split('.')[1] == 'jpg'})
     print('Index created.')
 
     if not os.path.isdir(dest):
@@ -29,26 +26,27 @@ def main(source, dest):
 
         for x in partition.keys():
             os.mkdir(os.path.join(dest, x))
-            os.mkdir(os.path.join(dest, 'target-' + x))
-            os.mkdir(os.path.join(dest, 'class-' + x))
+            os.mkdir(os.path.join(dest, x + '-target'))
         print('Directories created.')
 
     for x in partition.keys():
         print('Starting ' + x)
         for i in range(len(partition[x])):
             hash = (60 * i) // len(partition[x])
-            img = imread(partition[x][i])
-            if not os.path.exists(os.path.join(os.path.join(dest, x), partition[x][i].split(os.sep)[-1])):
-                imwrite(os.path.join(os.path.join(dest, x), partition[x][i].split(
-                    os.sep)[-1]), img_as_ubyte(color.rgb2gray(img)))
-            if not os.path.exists(os.path.join(os.path.join(dest, 'target-' + x), partition[x][i].split(os.sep)[-1])):
-                imwrite(os.path.join(os.path.join(dest, 'target-' + x),
-                                     partition[x][i].split(os.sep)[-1]), img)
-            if not os.path.exists(os.path.join(os.path.join(dest, 'class-' + x), partition[x][i].split(os.sep)[-1])):
-                imwrite(os.path.join(os.path.join(dest, 'class-' + x),
-                                     partition[x][i].split(os.sep)[-1]), img_as_ubyte(resize(img, (224, 224, 3))))
+
+            source = Image.open(partition[x][i])
+            source.thumbnail((224, 224), Image.LANCZOS)
+            img = Image.new('RGB', (224, 224))
+            img.paste(source, ((224 - source.size[0]) // 2, (224 - source.size[1]) // 2))
+
+            if not os.path.exists(os.path.join(os.path.join(dest, x + '-target'), partition[x][i].split(os.sep)[-1].split('.')[0] + '.png')):
+                img.save(os.path.join(os.path.join(dest, x + '-target'),
+                                      partition[x][i].split(os.sep)[-1].split('.')[0] + '.png'))
+            if not os.path.exists(os.path.join(os.path.join(dest, x), partition[x][i].split(os.sep)[-1].split('.')[0] + '.png')):
+                img.convert('L').save(os.path.join(os.path.join(dest, x),
+                                                   partition[x][i].split(os.sep)[-1].split('.')[0] + '.png'))
             print('{}[{}{}]{}%'.format(x, '#' * hash, ' ' * (60 - hash), (100 * i) // len(partition[x])), end='\r')
-        print('done.')
+        print('\ndone.')
     print('Dataset generated')
 
 
