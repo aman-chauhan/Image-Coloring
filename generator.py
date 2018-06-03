switch to categorical loss; rgb2lab conversion in generator; fixed tile method in FullNetwork; softmax loss for classifier

generator.py
@@ -1,4 +1,5 @@
 from keras.utils import Sequence, to_categorical
+from skimage.color import rgb2lab
 from imageio import imread
 import numpy as np
 import os
@@ -31,11 +32,12 @@ class DataGenerator(Sequence):
 
     def __data_generation(self, list_IDs_temp):
         X = np.empty((self.batch_size, 224, 224, self.n_channel))
-        Y = np.empty((self.batch_size, 224, 224, 3))
-        y = np.empty((self.batch_size), dtype=int)
+        Y = np.empty((self.batch_size, 224, 224, 2))
+        y = np.empty((self.batch_size,), dtype=int)
 
         for i, ID in enumerate(list_IDs_temp):
             X[i] = np.expand_dims(imread(os.path.join(os.path.join('data', self.partition), ID)), axis=-1)
-            Y[i] = imread(os.path.join(os.path.join('data', self.partition + '-target'), ID))
+            Y[i] = (rgb2lab(imread(os.path.join(os.path.join('data', self.partition + '-target'), ID)))
+                    [:, :, 1:] + 128.0) / (255.0)
             y[i] = self.labels[ID]
-            return ([X, X], [Y, to_categorical(y, num_classes=self.n_classes)])
+        return ([X, X], [Y, to_categorical(y, num_classes=self.n_classes)])

train.py
@@ -34,8 +34,8 @@ def main():
     json.dump(l, open('mapping.json', 'w'))
     print('Mappings written.')
 
-    training_generator = DataGenerator(partition['training'], 'training', labels, 16, 1, n_classes, True)
-    validation_generator = DataGenerator(partition['validation'], 'validation', labels, 16, 1, n_classes, True)
+    training_generator = DataGenerator(partition['training'], 'training', labels, 6, 1, n_classes, True)
+    validation_generator = DataGenerator(partition['validation'], 'validation', labels, 6, 1, n_classes, True)
 
     model = None
     with tf.device('/cpu:0'):
@@ -50,9 +50,9 @@ def main():
     cbk = SaveCallback(model)
     parallel_model = multi_gpu_model(model, gpus=2)
     parallel_model.compile(optimizer='adadelta', loss={
-        'color_model': 'mean_squared_error', 'clf_model': 'binary_crossentropy'})
+        'color_model': 'mean_squared_error', 'clf_model': 'categorical_crossentropy'}, metrics={'color_model': 'accuracy', 'clf_model': 'accuracy'})
     parallel_model.fit_generator(generator=training_generator, epochs=1000, callbacks=[
-                                 cbk], validation_data=validation_generator, use_multiprocessing=True, workers=4, initial_epoch=initial_epoch)
+        cbk], validation_data=validation_generator, use_multiprocessing=True, workers=4, initial_epoch=initial_epoch)
     print('Training done.')
 
 